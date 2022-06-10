ac4753027f50423af1763d16cb77ac57ff9abbc3
Jonas Bang <jonasbang@hotmail.dk>
2022-06-10 09:24:00 +0200

    Fixed bøsse routing

---
+++ components/TheLoginForm.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        <h1>Login Fomr</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
---
+++ pages/login.vue
@@ -0,0 +1,17 @@
+<template>
+    <div className="root">
+        <div className="container">
+            <TheLoginForm />
+        </div>
+    </div>
+</template>
+
+<script>
+import TheLoginForm from '../components/TheLoginForm.vue'
+
+export default {
+  components: {
+    TheLoginForm
+  }
+}
+</script>
